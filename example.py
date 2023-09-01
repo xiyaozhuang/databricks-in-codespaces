0fbdc11a1c6bcee45ed0489f16eec9fad294e241
xiyaozhuang <114228792+xiyaozhuang@users.noreply.github.com>
2023-09-01 14:55:29 +0000

    Added example scripts

---
+++ example.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+
+spark = SparkSession.builder.getOrCreate()
+
+schema = StructType(
+    [
+        StructField("CustomerID", IntegerType(), False),
+        StructField("FirstName", StringType(), False),
+        StructField("LastName", StringType(), False),
+    ]
+)
+
+data = [
+    [1000, "Mathijs", "Oosterhout-Rijntjes"],
+    [1001, "Joost", "van Brunswijk"],
+    [1002, "Stan", "Bokenkamp"],
+]
+
+df = spark.createDataFrame(data, schema)
+df.show()
